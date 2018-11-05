71e9fb33d3bda05dc0dc27f270fd9213d98227b2
Parag Dakle <parag.dakle@gmail.com>
2018-11-05 15:40:56 -0600

    Added Spark Velocity HTML Template dependency and a simple Hello World page.
    Also, fixed javax.servlet dependency issue.

--- src/main/java/routes/Router.java
+++ src/main/java/routes/Router.java
@@ -1,13 +1,31 @@
 package routes;
 
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.get;
 
 public class Router {
 
     public static void main(String[] args) {
+
+        //GET REST APIS
         get("/hello", (req, res) -> "Current time is " + new Date(System.currentTimeMillis()).toString());
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("message", "Hello World!");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, "templates/home.vm")
+            );
+        });
+
+        //POST REST APIS
+
     }
 
 }
